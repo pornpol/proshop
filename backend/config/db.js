4f0ef18f1d64acf2c88337e81a5eae57f47f4e96
pornpol <pornpol.w@gmail.com>
2020-10-11 20:10:53 +0700

    create data models

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,9 +8,9 @@ const connectDB = async () => {
       useCreateIndex: true,
     });
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
-    console.error(`Error: ${error}`);
+    console.error(`Error: ${error}`.red.underline.bold);
     process.exit(1);
   }
 };
